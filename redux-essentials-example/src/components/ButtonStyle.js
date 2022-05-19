4135db6c270fba06ac8591c099c8efd280fa52c6
josesison <josesison1@yahoo.com>
2022-05-19 13:56:31 -0700

    updated about, counter A and B

--- redux-essentials-example/src/components/ButtonStyle.js
+++ redux-essentials-example/src/components/ButtonStyle.js
@@ -4,9 +4,8 @@ const ButtonStyle = ({ text, click }) => {
   return (
     <button
       onClick={click}
-      className="w-60 h-15 p-2 border-solid border-2 border-orange-300 rounded-md bg-white text-black shadow-lg">
+      className="w-full h-15 p-2 border-solid border-2 border-orange-300 rounded-md bg-white text-black shadow-lg">
       {text}
-
     </button>
   )
 }
--- redux-essentials-example/src/components/CardContainer.js
+++ redux-essentials-example/src/components/CardContainer.js
@@ -2,9 +2,8 @@ import React from 'react'
 
 const CardContainer = ({ string, text, url }) => {
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg">
-      <img className="w-full" src={url} />
-      <div className="px-6 py-4">
+    <div className="flex justify-around border-2 border-white-100 max-w-sm rounded overflow-hidden shadow-lg">
+      <div className="px-10 py-4">
         <div className="font-bold text-xl mb-2">{string}</div>
         <p className="text-gray-700 text-base">
           {text}
