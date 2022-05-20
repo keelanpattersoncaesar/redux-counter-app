f3fe18a717091d4f6c8cfaf8b58d320f04978018
Tiffany Do <tiffanydoh@gmail.com>
2022-05-20 10:52:30 -0700

    finished counter app

--- redux-essentials-example/src/components/CounterA.js
+++ redux-essentials-example/src/components/CounterA.js
@@ -11,16 +11,17 @@ import {
   incrementAsync,
   incrementIfOdd,
   incrementIfEven,
-  selectCount,
+  selectCountA,
 } from '../features/counter/counterSlice';
-import { incrementByAmountB } from '../features/counter/counterSliceB';
+import { incrementByAmountB, selectCountB } from '../features/counter/counterSliceB';
 import ButtonStyle from './ButtonStyle'
 import ButtonStyleSmall from './ButtonStyleSmall';
 import { FiMinusCircle } from 'react-icons/fi'
 import { FiPlusCircle } from 'react-icons/fi'
 
 export function CounterA() {
-  const count = useSelector(selectCount);
+  const countA = useSelector(selectCountA);
+  const countB = useSelector(selectCountB);
   const dispatch = useDispatch();
   const [incrementAmount, setIncrementAmount] = useState('0');
 
@@ -37,7 +38,7 @@ export function CounterA() {
   const addIfOdd = () => {
     dispatch(incrementIfOdd(incrementValue))
   }
-  
+
   const addIfEven = () => {
     dispatch(incrementIfEven(incrementValue))
   }
@@ -57,43 +58,43 @@ export function CounterA() {
   return (
     <div className="App bg-orange-200 border-2 shadow-xl m-5 rounded-xl">
       <header className="App-header bg-transparent">
-        <div style={{height: 250, width: 250}} className="shadow-lg border-8 border-orange-400 rounded-full">
-          <img style={{height: "100%", width:"100%"}} src="https://i.ibb.co/yyv9frT/fruit.png" className="App-logo" alt="logo" />
+        <div style={{ height: 250, width: 250 }} className="shadow-lg border-8 border-orange-400 rounded-full">
+          <img style={{ height: "100%", width: "100%" }} src="https://i.ibb.co/yyv9frT/fruit.png" className="App-logo" alt="logo" />
         </div>
-    <div className='bg-transparent rounded-lg p-2'>
-        <input
-          className="w-20 border-solid border-2 border-orange-500 rounded-lg bg-orange-200 shadow-lg shadow-orange-500/50 text-center ..."
-          aria-label="Set increment amount"
-          value={incrementAmount}
-          onChange={(e) => setIncrementAmount(e.target.value)}
+        <div className='bg-transparent rounded-lg p-2'>
+          <input
+            className="w-20 border-solid border-2 border-orange-500 rounded-lg bg-orange-200 shadow-lg shadow-orange-500/50 text-center ..."
+            aria-label="Set increment amount"
+            value={incrementAmount}
+            onChange={(e) => setIncrementAmount(e.target.value)}
           />
-    <div className="flex flex-row items-center justify-center px-10">
-          <FiMinusCircle  
-          aria-label="Decrement value"
-          className="mx-10" 
-          onClick={() => dispatch(decrement())}
-          />
-        <span>{count}</span>
-        <FiPlusCircle
-          className="mx-10"
-          aria-label="Increment value"
-          onClick={() => dispatch(increment())}
-          />
-    </div>
-      <div className="flex flex-col items-center space-y-3">
-        <ButtonStyle text={"Add Amount"} click={addAmount} />
-        <ButtonStyle text={"Add Async"} click={addAsync} />
-      <div className="flex flex-row space-x-4">
-        <ButtonStyleSmall text={"Add If Odd"} click={addIfOdd} />
-        <ButtonStyleSmall text={"Add If Even"} click={addIfEven} />
-      </div>
-      <div className="flex flex-row space-x-4">
-        <ButtonStyleSmall text={"Add 5"} click={add5} />
-        <ButtonStyleSmall text={"Subtract 5"} click={subtract5} />
-      </div>
-      <ButtonStyle text={"Add Amount to B"} click={addAmountToB}/>
-    </div> 
-      </div>
+          <div className="flex flex-row items-center justify-center px-10">
+            <FiMinusCircle
+              aria-label="Decrement value"
+              className="mx-10"
+              onClick={() => dispatch(decrement())}
+            />
+            <span>{countA}</span>
+            <FiPlusCircle
+              className="mx-10"
+              aria-label="Increment value"
+              onClick={() => dispatch(increment())}
+            />
+          </div>
+          <div className="flex flex-col items-center space-y-3">
+            <ButtonStyle text={"Add Amount"} click={addAmount} />
+            <ButtonStyle text={"Add Async"} click={addAsync} />
+            <div className="flex flex-row space-x-4">
+              <ButtonStyleSmall text={"Add If Odd"} click={addIfOdd} />
+              <ButtonStyleSmall text={"Add If Even"} click={addIfEven} />
+            </div>
+            <div className="flex flex-row space-x-4">
+              <ButtonStyleSmall text={"Add 5"} click={add5} />
+              <ButtonStyleSmall text={"Subtract 5"} click={subtract5} />
+            </div>
+            <ButtonStyle text={`Add Amount to B (${countB})`} click={addAmountToB} />
+          </div>
+        </div>
       </header>
     </div>
   );
--- redux-essentials-example/src/features/counter/counterSlice.js
+++ redux-essentials-example/src/features/counter/counterSlice.js
@@ -68,19 +68,19 @@ export const { increment, decrement, incrementByAmountA, decrementBy5, increment
 // The function below is called a selector and allows us to select a value from
 // the state. Selectors can also be defined inline where they're used instead of
 // in the slice file. For example: `useSelector((state: RootState) => state.counter.value)`
-export const selectCount = (state) => state.counter.value;
+export const selectCountA = (state) => state.counter.value;
 
 // We can also write thunks by hand, which may contain both sync and async logic.
 // Here's an example of conditionally dispatching actions based on current state.
 export const incrementIfOdd = (amount) => (dispatch, getState) => {
-  const currentValue = selectCount(getState());
+  const currentValue = selectCountA(getState());
   if (currentValue % 2 === 1) {
     dispatch(incrementByAmountA(amount));
   }
 };
 
 export const incrementIfEven = (amount) => (dispatch, getState) => {
-  const currentValue = selectCount(getState());
+  const currentValue = selectCountA(getState());
   if (currentValue % 2 === 0) {
     dispatch(incrementByAmountA(amount));
   }
